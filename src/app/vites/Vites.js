38f642ef9d4f01da821c501eec918f1fc420bdd2
jhserodio <jhserodio@gmail.com>
2017-03-24 21:33:35 -0300

    Vites

---
+++ src/app/vites/Vites.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const styleVites = {
+  background: 'black',
+  color: 'white',
+}
+
+const styleLogo = {
+  margin: 0,
+  padding: '16px',
+  fontSize: '14px',
+  textTransform: 'uppercase'
+}
+
+const Vites = props => (
+  <div style={styleVites}>
+    <h1 style={styleLogo}>
+      Vites
+    </h1>
+  </div>
+);
+
+export default Vites;
